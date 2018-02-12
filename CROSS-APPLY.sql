19ed65bf5e03d4eb96413f08f45b3e00f6d27fc1
Bambam Deo <bambamdeo@users.noreply.github.com>
2018-02-13 00:03:30 +0530

    Create CROSS-APPLY.sql

---
+++ CROSS-APPLY.sql
@@ -0,0 +1,94 @@
+USE [tempdb] 
+GO
+ 
+IF EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID(N'[Employee]') AND type IN (N'U')) 
+BEGIN 
+   DROP TABLE [Employee] 
+END 
+GO 
+
+IF EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID(N'[Department]') AND type IN (N'U')) 
+BEGIN 
+   DROP TABLE [Department] 
+END 
+CREATE TABLE [Department]( 
+   [DepartmentID] [int] NOT NULL PRIMARY KEY, 
+   [Name] VARCHAR(250) NOT NULL, 
+) ON [PRIMARY] 
+INSERT [Department] ([DepartmentID], [Name])  
+VALUES (1, N'Engineering') 
+INSERT [Department] ([DepartmentID], [Name])  
+VALUES (2, N'Administration') 
+INSERT [Department] ([DepartmentID], [Name])  
+VALUES (3, N'Sales') 
+INSERT [Department] ([DepartmentID], [Name])  
+VALUES (4, N'Marketing') 
+INSERT [Department] ([DepartmentID], [Name])  
+VALUES (5, N'Finance') 
+GO 
+
+CREATE TABLE [Employee]( 
+   [EmployeeID] [int] NOT NULL PRIMARY KEY, 
+   [FirstName] VARCHAR(250) NOT NULL, 
+   [LastName] VARCHAR(250) NOT NULL, 
+   [DepartmentID] [int] NOT NULL REFERENCES [Department](DepartmentID), 
+) ON [PRIMARY] 
+GO
+ 
+INSERT [Employee] ([EmployeeID], [FirstName], [LastName], [DepartmentID]) 
+VALUES (1, N'Orlando', N'Gee', 1 ) 
+INSERT [Employee] ([EmployeeID], [FirstName], [LastName], [DepartmentID]) 
+VALUES (2, N'Keith', N'Harris', 2 ) 
+INSERT [Employee] ([EmployeeID], [FirstName], [LastName], [DepartmentID]) 
+VALUES (3, N'Donna', N'Carreras', 3 ) 
+INSERT [Employee] ([EmployeeID], [FirstName], [LastName], [DepartmentID]) 
+VALUES (4, N'Janet', N'Gates', 3 ) 
+
+-- CROSS APPLY and INNER JOIN
+
+SELECT * FROM Department D 
+CROSS APPLY ( 
+   SELECT * FROM Employee E 
+   WHERE E.DepartmentID = D.DepartmentID 
+   ) A 
+GO
+ 
+SELECT * FROM Department D 
+INNER JOIN Employee E ON D.DepartmentID = E.DepartmentID 
+GO 
+
+-- OUTER APPLY and LEFT OUTER JOIN
+
+SELECT * FROM Department D 
+OUTER APPLY ( 
+   SELECT * FROM Employee E 
+   WHERE E.DepartmentID = D.DepartmentID 
+   ) A 
+GO
+ 
+SELECT * FROM Department D 
+LEFT OUTER JOIN Employee E ON D.DepartmentID = E.DepartmentID 
+GO 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM Customers C
+        INNER JOIN Orders O ON C.CustomerId = O.CustomerId
+--equilvalent to
+SELECT * FROM Customers C
+  CROSS APPLY (SELECT * FROM Orders O
+               WHERE O.CustomerId = C.CustomerId) ORD
+               
+
