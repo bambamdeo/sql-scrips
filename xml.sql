e382ed5577aab882c9ad052766eb1eeecb6fa3c2
Bambam Deo <bambamdeo@users.noreply.github.com>
2018-02-13 01:12:02 +0530

    Update xml.sql

--- xml.sql
+++ xml.sql
@@ -1,3 +1,5 @@
+DECLARE @idoc int, @doc varchar(1000);  
+
 SET @doc ='  
 <ROOT>  
 <Customer CustomerID="VINET" ContactName="Paul Henriot">  
@@ -12,8 +14,6 @@ SET @doc ='
    </Order>  
 </Customer>  
 </ROOT>';  
-
-DECLARE @idoc int, @doc varchar(1000);  
 
 --Create an internal representation of the XML document.  
 EXEC sp_xml_preparedocument @idoc OUTPUT, @doc; 
@@ -41,3 +41,39 @@ FROM   OPENXML (@idoc, '/ROOT/Customer/Order/OrderDetail',2)
 
 EXEC sp_xml_removedocument @idoc 
 
+--*******************************************************************************************************************
+
+CREATE TABLE Customers (CustomerID varchar(20) primary key,  
+                ContactName varchar(20),   
+                CompanyName varchar(20));  
+GO  
+CREATE TABLE Orders( CustomerID varchar(20), OrderDate datetime;)  
+GO  
+DECLARE @docHandle int;  
+DECLARE @xmlDocument nvarchar(max); -- or xml type  
+SET @xmlDocument = N'<ROOT>  
+<Customers CustomerID="XYZAA" ContactName="Joe" CompanyName="Company1">  
+<Orders CustomerID="XYZAA" OrderDate="2000-08-25T00:00:00"/>  
+<Orders CustomerID="XYZAA" OrderDate="2000-10-03T00:00:00"/>  
+</Customers>  
+<Customers CustomerID="XYZBB" ContactName="Steve"  
+CompanyName="Company2">No Orders yet!  
+</Customers>  
+</ROOT>';  
+
+EXEC sp_xml_preparedocument @docHandle OUTPUT, @xmlDocument;  
+-- Use OPENXML to provide rowset consisting of customer data.  
+INSERT Customers   
+SELECT *   
+FROM OPENXML(@docHandle, N'/ROOT/Customers')   
+  WITH Customers;  
+-- Use OPENXML to provide rowset consisting of order data.  
+INSERT Orders   
+SELECT *   
+FROM OPENXML(@docHandle, N'//Orders')   
+  WITH Orders;  
+-- Using OPENXML in a SELECT statement.  
+SELECT * FROM OPENXML(@docHandle, N'/ROOT/Customers/Orders') WITH (CustomerID nchar(5) '../@CustomerID', OrderDate datetime);  
+
+-- Remove the internal representation of the XML document.  
+EXEC sp_xml_removedocument @docHandle;
