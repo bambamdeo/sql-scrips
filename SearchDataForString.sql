a2bb90da422e4ac07346e20f10b1a7b6cb651c37
Bambam Deo <bambamdeo@users.noreply.github.com>
2018-03-28 13:32:48 +0530

    Update SearchDataForString.sql

--- SearchDataForString.sql
+++ SearchDataForString.sql
@@ -1,15 +1,6 @@
 DECLARE @SearchStr nvarchar(100)
-SET @SearchStr = '## YOUR STRING HERE ##'
--- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
--- Purpose: To search all columns of all tables for a given search string
--- Written by: Narayana Vyas Kondreddi
--- Site: http://vyaskn.tripod.com
--- Updated and tested by Tim Gaunt
--- http://www.thesitedoctor.co.uk
--- http://blogs.thesitedoctor.co.uk/tim/2010/02/19/Search+Every+Table+And+Field+In+A+SQL+Server+Database+Updated.aspx
--- Tested on: SQL Server 7.0, SQL Server 2000, SQL Server 2005 and SQL Server 2010
--- Date modified: 03rd March 2011 19:00 GMT
-CREATE TABLE #Results (ColumnName nvarchar(370), ColumnValue nvarchar(3630))
+SET @SearchStr = '--str2search--'
+CREATE TABLE #Results (TableName nvarchar(370),ColumnName nvarchar(370), ColumnValue nvarchar(3630))
  
 SET NOCOUNT ON
  
@@ -53,13 +44,24 @@ BEGIN
             INSERT INTO #Results
             EXEC
             (
-                'SELECT ''' + @TableName + '.' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) FROM ' + @TableName + ' (NOLOCK) ' +
+                'SELECT ''' + @TableName + ''',''' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) FROM ' + @TableName + ' (NOLOCK) ' +
                 ' WHERE ' + @ColumnName + ' LIKE ' + @SearchStr2
             )
         END
     END   
 END
  
-SELECT ColumnName, ColumnValue FROM #Results
+SELECT DISTINCT TableName, ColumnName FROM #Results
+SELECT * FROM #Results
  
 DROP TABLE #Results
+--SRC--
+-- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
+-- Purpose: To search all columns of all tables for a given search string
+-- Written by: Narayana Vyas Kondreddi
+-- Site: http://vyaskn.tripod.com
+-- Updated and tested by Tim Gaunt
+-- http://www.thesitedoctor.co.uk
+-- http://blogs.thesitedoctor.co.uk/tim/2010/02/19/Search+Every+Table+And+Field+In+A+SQL+Server+Database+Updated.aspx
+-- Tested on: SQL Server 7.0, SQL Server 2000, SQL Server 2005 and SQL Server 2010
+-- Date modified: 03rd March 2011 19:00 GMT
