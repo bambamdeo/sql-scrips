95adc1f0e4eb9712ba4df1045d3a5eaf6771364b
Bambam Deo <bambamdeo@users.noreply.github.com>
2018-07-10 18:28:49 +0530

    Update fragmentation.sql

--- fragmentation.sql
+++ fragmentation.sql
@@ -1,3 +1,21 @@
+
+SELECT dbschemas.[name] as 'Schema',
+dbtables.[name] as 'Table',
+dbindexes.[name] as 'Index',
+indexstats.avg_fragmentation_in_percent,
+indexstats.page_count
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
+INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
+INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
+INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
+AND indexstats.index_id = dbindexes.index_id
+WHERE indexstats.database_id = DB_ID()
+ORDER BY indexstats.page_count desc, indexstats.avg_fragmentation_in_percent desc
+
+
+--OR
+
+
 SELECT OBJECT_NAME(ind.OBJECT_ID) AS TableName, 
 ind.name AS IndexName, indexstats.index_type_desc AS IndexType, 
 indexstats.avg_fragmentation_in_percent 
