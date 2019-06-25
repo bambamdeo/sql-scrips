b9937fca1708379cb9870d6c6e84f79fac26e32c
Bambam Deo <bambamdeo@users.noreply.github.com>
2019-06-25 13:52:57 +0530

    size of tables added

---
+++ rows_count_or_size_by_table.sql
@@ -0,0 +1,40 @@
+SELECT sc.name +'.'+ ta.name TableName
+ ,SUM(pa.rows) RowCnt
+ FROM sys.tables ta
+ INNER JOIN sys.partitions pa
+ ON pa.OBJECT_ID = ta.OBJECT_ID
+ INNER JOIN sys.schemas sc
+ ON ta.schema_id = sc.schema_id
+ WHERE ta.is_ms_shipped = 0 AND pa.index_id IN (1,0)
+ GROUP BY sc.name,ta.name
+ ORDER BY SUM(pa.rows) DESC
+
+
+SELECT 
+    t.NAME AS TableName,
+    s.Name AS SchemaName,
+    p.rows AS RowCounts,
+    SUM(a.total_pages) * 8 AS TotalSpaceKB, 
+    CAST(ROUND(((SUM(a.total_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS TotalSpaceMB,
+    SUM(a.used_pages) * 8 AS UsedSpaceKB, 
+    CAST(ROUND(((SUM(a.used_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS UsedSpaceMB, 
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceKB,
+    CAST(ROUND(((SUM(a.total_pages) - SUM(a.used_pages)) * 8) / 1024.00, 2) AS NUMERIC(36, 2)) AS UnusedSpaceMB
+FROM 
+    sys.tables t
+INNER JOIN      
+    sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN 
+    sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN 
+    sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN 
+    sys.schemas s ON t.schema_id = s.schema_id
+WHERE 
+    t.NAME NOT LIKE 'dt%' 
+    AND t.is_ms_shipped = 0
+    AND i.OBJECT_ID > 255 
+GROUP BY 
+    t.Name, s.Name, p.Rows
+ORDER BY 
+    TotalSpaceKB desc,t.Name
